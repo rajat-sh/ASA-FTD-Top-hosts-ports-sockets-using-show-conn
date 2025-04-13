27987dc313049db4f66208458ec20a98f10a13d2
rajat-sh <39512057+rajat-sh@users.noreply.github.com>
2025-04-13 23:24:55 +0200

    Add files via upload

--- tophost.sh
+++ tophost.sh
@@ -1,87 +1,91 @@
-#!/bin/bash
-
-# Prompt the user for the input file
-read -p "Enter the input file name: " input_file
-read -p "Enter the number of top hosts, ports, sockets, IP pair required: " nums
-
-# AWK script to process the file
-awk '
-BEGIN {
-    print "Processing lines starting with TCP, UDP, or ICMP...";
-}
-
-/^TCP|^UDP|^ICMP/ {
-    #print $0;  # Print the entire matching line
-    #print "Third column:", $3;  # Print the third column
-    #print "Fifth column:", $5;  # Print the fifth column
-    array1[length(array1) + 1] = $3;  # Store third column in array1
-    array1[length(array1) + 1] = $5;  # Store fifth column in array1
-}
-
-END {
-    #print "Array1 Elements (before removing commas):";
-    for (i = 1; i <= length(array1); i++) {
-        #print array1[i];
-    }
-    
-    #print "Array1 Elements (after removing commas):";
-    for (i = 1; i <= length(array1); i++) {
-        gsub(",", "", array1[i]);  # Remove commas
-        #print array1[i];
-    }
-
-    # Count unique substrings before ":"
-    #print "Unique substrings before colon:";
-    for (i = 1; i <= length(array1); i++) {
-        split(array1[i], arr, ":");
-        pre[arr[1]]++;
-    }
-    for (key in pre) print key, pre[key] > "ip.temp";
-
-    # Count unique substrings after ":"
-    #print "Unique substrings after colon:";
-    for (i = 1; i <= length(array1); i++) {
-        split(array1[i], arr, ":");
-        post[arr[2]]++;
-    }
-    for (key in post) print key, post[key] > "ports.temp";
-
-    # Count unique elements
-    #print "Unique elements in array1:";
-    for (i = 1; i <= length(array1); i++) {
-        unique[array1[i]]++;
-    }
-    for (key in unique) print key, unique[key] > "sockets.temp";
-
-    # Concatenate every alternate element
-    #print "Unique concatenated substrings:";
-    count = 0;
-    for (i = 1; i <= length(array1); i++) {
-        split(array1[i], arr, ":");
-        if (count % 2 == 0) {
-            concatenated[arr[1] "<-------->" arr[1]]++;
-        }
-        count++;
-    }
-    for (key in concatenated) print key, concatenated[key] > "ippair.temp";
-}
-' "$input_file"
-cat ip.temp | sort -nrk1 | head -$nums > ip.temp1
-cat ports.temp | sort -nrk1 | head -$nums > ports.temp1
-cat sockets.temp | sort -nrk1 | head -$nums > sockets.temp1
-cat ippair.temp | sort -nrk1 | head -$nums > ippair.temp1
-
-echo "Top $nums IP Addresses"
-cat ip.temp1
-
-echo "Top $nums Ports"
-cat ports.temp1
-
-echo "Top $nums Sockets"
-cat sockets.temp1
-
-echo "Top $nums IP Addresses pairs"
-cat ippair.temp1
-
-rm ip.temp ip.temp1 ports.temp ports.temp1 sockets.temp sockets.temp1 ippair.temp ippair.temp1
-
+echo "Enter the show conn file name in current directory or full path name"
+ read filename
+ echo "Enter the number of top hosts, ports, sockets, IP pair required"
+ read nums
+ 
+ echo "Top IP ADDRESSs" > ip.temp
+ echo "Top PORTS" > port.temp
+ echo "Top SOCKETS" > socket.temp
+ echo "Top PAIR of ADDRESS" > pair.temp
+ 
+ cat $filename | awk 'BEGIN{OFS = "\n"}
+ {
+ 	#Next line is removing the commas from each line
+ 	gsub(",", "", $0)
+ 	#Next line is checking if the first column value is TCP,UDP,ICMP,GRE as for portless connections such as EIGRP etc column will be different. 
+ 	if($1 == "TCP" || $1 == "UDP" || $1 == "ICMP" || $1 == "GRE")
+ 	{
+ 		#Next two line removes the ":" between the IP address and ports.
+ 		 sub(":", " ", $0)
+ 		 sub(":", " ", $0)
+ 		print $3,$6
+ 	}
+ 	else
+ 	{	#Next line is printing the IP address in connections without ports such as EIGRP
+ 		print $3,$5
+ 	}
+ }
+ END{}' | awk 'BEGIN{}
+ {a[$1]++}
+ END{for(x in a)print a[x]" "x}
+ ' | sort -rnk1 | head -$nums >> ip.temp &
+ cat $filename | awk 'BEGIN{OFS = "\n"}
+ {
+ 	#Next line is removing the commas from each line
+ 	gsub(",", "", $0)
+ 	#Next line is checking if the first column value is TCP,UDP as this would be most common use case
+ 	if($1 == "TCP" || $1 == "UDP")
+ 	{
+ 		#Next two line removes the ":" between the IP address and ports.
+ 		 sub(":", " ", $0)
+ 		 sub(":", " ", $0)
+ 		print $4,$7
+ 	}
+ }
+ END{} '  | awk 'BEGIN{}
+ {a[$1]++}
+ END{for(x in a)print a[x]" "x}
+ ' | sort -rnk1 | head -$nums >> port.temp &
+  cat $filename | awk 'BEGIN{OFS = "\n"}
+ {
+ 	#Next line is removing the commas from each line
+ 	gsub(",", "", $0)
+ 	#Next line is checking if the first column value is TCP,UDP most common case
+ 	if($1 == "TCP" || $1 == "UDP")
+ 	{
+ 		#Next two line removes the ":" between the IP address and ports.
+ 		 sub(":", " ", $0)
+ 		 sub(":", " ", $0)
+ 		print $3":"$4
+ 	}
+ }
+ END{}
+ ' | awk 'BEGIN{}
+ {a[$1]++}
+ END{for(x in a)print a[x]" "x}
+ ' | sort -nrk1 | head -$nums >> socket.temp &
+ cat $filename | awk 'BEGIN{OFS = "\n"}
+ {
+ 	#Next line is removing the commas from each line
+ 	gsub(",", "", $0)
+ 	#Next line is checking if the first column value is TCP,UDP,ICMP,GRE as for portless connections such as EIGRP etc column will be different. 
+ 	if($1 == "TCP" || $1 == "UDP" || $1 == "ICMP" || $1 == "GRE")
+ 	{
+ 		#Next two line removes the ":" between the IP address and ports.
+ 		 sub(":", " ", $0)
+ 		 sub(":", " ", $0)
+ 		print $3"<->"$6
+ 	}
+ 	else
+ 	{	#Next line is printing the IP address pair in connections without ports such as EIGRP
+ 		print $3"<->"$5
+ 	}
+ }
+ END{}' | awk 'BEGIN{}
+ {a[$1]++}
+ END{for(x in a)print a[x]" "x}
+ ' | sort -nrk1 | head -$nums >> pair.temp &
+ 
+ wait
+ cat ip.temp port.temp socket.temp pair.temp
+ rm ip.temp port.temp socket.temp pair.temp
